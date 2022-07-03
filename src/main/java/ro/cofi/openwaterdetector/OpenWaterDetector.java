88fcca4d1ccdc311ca7b2a0ef668f702f03f0368
CofiPRT <raresgsmart@gmail.com>
2022-07-03 15:41:21 +0300

    Initial commit

---
+++ src/main/java/ro/cofi/openwaterdetector/OpenWaterDetector.java
@@ -0,0 +1,10 @@
+package ro.cofi.openwaterdetector;
+
+import net.fabricmc.api.ModInitializer;
+
+public class OpenWaterDetector implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
